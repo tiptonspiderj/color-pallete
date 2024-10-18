234f1214334717b28259619aa125638d061c3142
JEREMY TIPTON <32341894+tiptonspiderj@users.noreply.github.com>
2024-10-18 14:58:51 -0400

    Fixed id errors

--- index.js
+++ index.js
@@ -8,20 +8,20 @@ const colorCode = document.querySelectorAll("#color-code-container div")
 
 myButton.addEventListener("click", function() {
   //remove the # symbol from the color value for the API string
-  const colors = colorPicker.value.slice(1)
+  const myColor = colorPicker.value.slice(1)
   const scheme = colorScheme.value
-  const url = `https://www.thecolorapi.com/scheme?hex=${colors}&format=json&mode=${scheme}`
+  const url = `https://www.thecolorapi.com/scheme?hex=${myColor}&format=json&mode=${scheme}`
   fetch(url)
   .then(res => res.json())
   .then(colorsArray => {
     let swatches = ""
     let hexValues = ""
-    for (color of colorsArray.colors) {
+    for (let color of colorsArray.colors) {
       swatches += `
-          <div id="color-Swatch" style="background-color:${color.hex.value};"></div>
+          <div id="color-Swatch-${color.hex.value}" style="background-color:${color.hex.value};"></div>
       `
       hexValues +=`
-          <div id="hex-Value-Text">${color.hex.value}</div>
+          <div class="hex-Value-Text">${color.hex.value}</div>
       `
     }
     colorSwatches.innerHTML = swatches
@@ -29,7 +29,7 @@ myButton.addEventListener("click", function() {
     
     for (let i = 0; i < colorsArray.colors.length; i++) {
       const color = colorsArray.colors[i].hex.value;
-      colorCode[i].setAttribute("data-color", color);
+      colorCodes.children[i].setAttribute("data-color", color);
     }
   })
 })
